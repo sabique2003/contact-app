eb950d71930a3a4bd1ab4152a761d287f3e9cbf4
sabique2003 <159254150+sabique2003@users.noreply.github.com>
2024-11-08 17:57:19 +0530

    Update allApis.js

--- src/Services/allApis.js
+++ src/Services/allApis.js
@@ -14,24 +14,21 @@ export const deleteContact = async (id) => {
   return await axios.delete(`${baseurl}/contacts/${id}`);
 };
 
-// New function to update contact
+
 export const updateContact = async (id, data) => {
-  return await axios.put(`${baseurl}/contacts/${id}`, data); // Update contact by ID
+  return await axios.put(`${baseurl}/contacts/${id}`, data); 
 };
 
-// Add to Favourites
+
 export const addToFavourites = async (contact) => {
   try {
-    // Check if the contact already exists in the favourites
     const response = await axios.get(`${baseurl}/favourites/${contact.id}`);
     if (response.data) {
       throw new Error("Contact is already in favourites");
     }
-    // Add the contact to favourites
     return await axios.post(`${baseurl}/favourites`, contact);
   } catch (error) {
     if (error.response && error.response.status === 404) {
-      // If the contact is not found, add it to favourites
       return await axios.post(`${baseurl}/favourites`, contact);
     } else {
       throw error;
@@ -39,12 +36,10 @@ export const addToFavourites = async (contact) => {
   }
 };
 
-// Remove from Favourites
 export const removeFromFavourites = async (id) => {
   return await axios.delete(`${baseurl}/favourites/${id}`);
 };
 
-// Get Favourites List
 export const getFavourites = async () => {
   return await axios.get(`${baseurl}/favourites`);
 };
